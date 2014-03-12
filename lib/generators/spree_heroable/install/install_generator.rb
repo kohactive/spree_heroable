449827cf68268dc4143c5ca8871c96729bf4c1e9
John Koht <john@kohactive.com>
2014-03-12 14:30:04 -0500

    remove store js/css from installer

--- lib/generators/spree_heroable/install/install_generator.rb
+++ lib/generators/spree_heroable/install/install_generator.rb
@@ -5,12 +5,10 @@ module SpreeHeroable
       class_option :auto_run_migrations, :type => :boolean, :default => false
 
       def add_javascripts
-        append_file 'app/assets/javascripts/store/all.js', "//= require store/spree_heroable\n"
         append_file 'app/assets/javascripts/admin/all.js', "//= require admin/spree_heroable\n"
       end
 
       def add_stylesheets
-        inject_into_file 'app/assets/stylesheets/store/all.css', " *= require store/spree_heroable\n", :before => /\*\//, :verbose => true
         inject_into_file 'app/assets/stylesheets/admin/all.css', " *= require admin/spree_heroable\n", :before => /\*\//, :verbose => true
       end
 
